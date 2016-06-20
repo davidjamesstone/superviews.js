e9c765c3c04294824e8f9d3b3ae6a44a19253218
David Stone <davidjamesstone@gmail.com>
2016-06-20 22:59:52 +0100

    Client work

--- examples/custom-elements/index.js
+++ examples/custom-elements/index.js
@@ -43,7 +43,53 @@ var MyElement = document.registerElement(
   }
 )
 
-
-var myElement = new MyElement()
-
-document.body.appendChild(myElement)
+var MySubElement = document.registerElement(
+  'my-subelement',
+  {
+    prototype: Object.create(
+      window.HTMLElement.prototype, {
+        something: {
+          get: function () {
+            return this._something
+          },
+          set: function (value) {
+            this._something = value
+          }
+        },
+        createdCallback: {value: function () {
+          window.console.log('here I am ^_^ ')
+          window.console.log('with content: ', this.textContent)
+          patch(this, view, {
+            name: 'Egg'
+          })
+        }},
+        attachedCallback: {value: function () {
+          window.console.log('live on DOM ;-) ')
+        }},
+        detachedCallback: {value: function () {
+          window.console.log('leaving the DOM :-( )')
+        }},
+        attributeChangedCallback: {
+          value: function (name, previousValue, value) {
+            if (previousValue == null) {
+              console.log(
+                'got a new attribute ', name,
+                ' with value ', value
+              )
+            } else if (value == null) {
+              console.log(
+                'somebody removed ', name,
+                ' its value was ', previousValue
+              )
+            } else {
+              console.log(
+                name,
+                ' changed from ', previousValue,
+                ' to ', value
+              )
+            }
+          }
+        }
+      })
+  }
+)
